9671602340a1171a36c539894a8e76554d80d481
Sitnikova Yekaterina <sitkat03@mail.ru>
2023-05-23 21:11:14 +0300

    add repeat new password for profile_screen.dart

--- lib/feature/auth/ui/profile_screen.dart
+++ lib/feature/auth/ui/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vortaro/app/domain/app_notifications.dart';
 import 'package:vortaro/app/domain/error_entity/error_entity.dart';
 import 'package:vortaro/app/ui/app_loader.dart';
+import 'package:vortaro/app/ui/components/app_dialog_three_params.dart';
 import 'package:vortaro/app/ui/components/app_dialog_two_params.dart';
 import 'package:vortaro/app/ui/components/app_snack_bar.dart';
 
@@ -22,85 +23,86 @@ class ProfileScreen extends StatelessWidget {
         ),
         automaticallyImplyLeading: false,
       ),
-      body: BlocConsumer<AuthCubit, AuthState>(listener: (context, state) {
-        state.whenOrNull(
-          authorized: (userEntity) {
-            if (userEntity.userState?.hasData == true) {
-              AppSnackBar.showSnackBarWithMessage(
-                  context, userEntity.userState?.data);
-            }
-            if (userEntity.userState?.hasError == true) {
-              AppSnackBar.showSnackBarWithError(context,
-                  ErrorEntity.fromException(userEntity.userState?.error));
-            }
-          },
-        );
-      }, builder: (context, state) {
-        final userEntity =
-            state.whenOrNull(authorized: (userEntity) => userEntity);
-        if (userEntity?.userState?.connectionState == ConnectionState.waiting) {
-          return const AppLoader();
-        }
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 65,
-                    backgroundColor: Colors.lightGreenAccent,
-                    child: CircleAvatar(
-                      radius: 60,
-                      backgroundColor: Colors.white,
-                      backgroundImage: AssetImage(
-                        'assets/images/default_user.png',
+      body: SingleChildScrollView(
+        child: BlocConsumer<AuthCubit, AuthState>(listener: (context, state) {
+          state.whenOrNull(
+            authorized: (userEntity) {
+              if (userEntity.userState?.hasData == true) {
+                AppSnackBar.showSnackBarWithMessage(
+                    context, userEntity.userState?.data);
+              }
+              if (userEntity.userState?.hasError == true) {
+                AppSnackBar.showSnackBarWithError(context,
+                    ErrorEntity.fromException(userEntity.userState?.error));
+              }
+            },
+          );
+        }, builder: (context, state) {
+          final userEntity =
+              state.whenOrNull(authorized: (userEntity) => userEntity);
+          if (userEntity?.userState?.connectionState == ConnectionState.waiting) {
+            return const AppLoader();
+          }
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 65,
+                      backgroundColor: Colors.lightGreenAccent,
+                      child: CircleAvatar(
+                        radius: 60,
+                        backgroundColor: Colors.white,
+                        backgroundImage: AssetImage(
+                          'assets/images/default_user.png',
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 45),
-                      Text(
-                        userEntity?.username ?? "",
-                        style: const TextStyle(
-                          fontFamily: "Muli",
-                          fontSize: 24,
-                          color: Color(0xff070707),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 45),
+                        Text(
+                          userEntity?.username ?? "",
+                          style: const TextStyle(
+                            fontFamily: "Muli",
+                            fontSize: 24,
+                            color: Color(0xff070707),
+                          ),
                         ),
-                      ),
-                      Text(
+                        Text(
                           userEntity?.email ?? "",
-                        style: TextStyle(
-                          fontFamily: "Muli",
-                          fontSize: 16,
-                          color: Colors.grey.shade700,
+                          style: TextStyle(
+                            fontFamily: "Muli",
+                            fontSize: 16,
+                            color: Colors.grey.shade700,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const Divider(color: Colors.grey),
-              TextButton(
+                      ],
+                    ),
+                  ],
+                ),
+                const Divider(color: Colors.grey),
+                TextButton(
                   onPressed: () {
                     showDialog(
                         context: context,
                         builder: (context) => AppDialogTwoParams(
-                          val1: "Почта",
-                          val2: "Логин",
-                          onPressed: (v1, v2) {
-                            context.read<AuthCubit>().userUpdate(
-                              email: v1,
-                              username: v2,
-                            );
-                          },
-                        ));
+                              val1: "Почта",
+                              val2: "Логин",
+                              onPressed: (v1, v2) {
+                                context.read<AuthCubit>().userUpdate(
+                                      email: v1,
+                                      username: v2,
+                                    );
+                              },
+                            ));
                   },
                   child: Row(
                     children: [
@@ -112,22 +114,29 @@ class ProfileScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-              ),
-              const Divider(color: Colors.grey),
-              TextButton(
+                ),
+                const Divider(color: Colors.grey),
+                TextButton(
                   onPressed: () {
                     showDialog(
                         context: context,
-                        builder: (context) => AppDialogTwoParams(
-                          val1: "Пароль",
-                          val2: "Новый пароль",
-                          onPressed: (v1, v2) {
-                            context.read<AuthCubit>().passwordUpdate(
-                              oldPassword: v1,
-                              newPassword: v2,
-                            );
-                          },
-                        ));
+                        builder: (context) => AppDialogThreeParams(
+                              val1: "Текущий пароль",
+                              val2: "Новый пароль",
+                              val3: "Повторите новый пароль",
+                              onPressed: (v1, v2, v3) {
+                                if (v2 != v3) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text("Пароли не совпадают")));
+                                } else {
+                                  context.read<AuthCubit>().passwordUpdate(
+                                        oldPassword: v1,
+                                        newPassword: v2,
+                                      );
+                                }
+                              },
+                            ));
                   },
                   child: Row(
                     children: [
@@ -139,32 +148,33 @@ class ProfileScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-              ),
-              const Divider(color: Colors.grey),
-              TextButton(
-                onPressed: () {
-                  AppNotifications().cancelAllNotifications();
-                  context.read<AuthCubit>().logOut();
-                },
-                child: Row(
-                  children: const [
-                    Icon(Icons.exit_to_app, color: Colors.black),
-                    SizedBox(width: 5),
-                    Text(
-                      "Выйти",
-                      style: TextStyle(
-                        fontFamily: "Muli",
-                        fontSize: 16,
-                          color: Colors.black,
-                      ),
-                    ),
-                  ],
                 ),
-              ),
-            ],
-          ),
-        );
-      }),
+                const Divider(color: Colors.grey),
+                TextButton(
+                  onPressed: () {
+                    AppNotifications().cancelAllNotifications();
+                    context.read<AuthCubit>().logOut();
+                  },
+                  child: Row(
+                    children: const [
+                      Icon(Icons.exit_to_app, color: Colors.black),
+                      SizedBox(width: 5),
+                      Text(
+                        "Выйти",
+                        style: TextStyle(
+                          fontFamily: "Muli",
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
